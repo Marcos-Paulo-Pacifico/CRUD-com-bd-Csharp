b29aa8361b2de3ac97f3c65ceb3810f0d91c6850
Paulocas <marcospacifico@usp.br>
2022-08-29 12:12:17 -0300

    atualizando para ler um banco de dados

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace read_write_files.Models
 {
-    public class Product : Base, IProduct
+    public class Product 
     {
         public string IdProduct { get; set; } = string.Empty;
 
@@ -17,56 +17,6 @@ namespace read_write_files.Models
 
         public decimal Price { get; set; }
 
-        private const string path = "database/products.csv";
 
-        public Product()
-        {
-            CreateFolderFile(path);
-        }
-
-        private static string PrepareLineCSV(Product product)
-        {
-            return $"{product.IdProduct};{product.Name};{product.Description};{product.Price}";
-        }
-
-        public List<Product> ReadAll()
-        {
-            List<Product> products = new();
-
-            string[] lines = File.ReadAllLines(path);
-
-            foreach (var item in lines)
-            {
-                string[] line = item.Split(";");
-
-                Product product = new()
-                {
-                    IdProduct = line[0],
-                    Name = line[1],
-                    Description = line[2],
-                    Price = Convert.ToDecimal( line[3] )
-                };
-
-                products.Add(product);
-            }
-
-            return products;
-        }
-
-        public void Create(Product newProduct)
-        {
-            string[] line = { PrepareLineCSV(newProduct) };
-            File.AppendAllLines(path, line);
-        }
-
-        public void Update(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(string idProduct)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
